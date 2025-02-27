df3b817f58f3ca199d3018ce843bdcad354ac902
Taras Dubyk <tarass.dubyk@gmail.com>
2025-02-27 15:40:29 +0200

    re from file init

---
+++ reverse_engineering/helpers/parseSchema.js
@@ -0,0 +1,32 @@
+/**
+ * @import { DocumentNode, GraphQLError } from "graphql"
+ */
+
+const { parse } = require('graphql');
+
+/**
+ * Parses GraphQL schema content into AST and validates it
+ * @param {Object} params
+ * @param {string} params.schemaContent - The GraphQL schema content to parse
+ * @returns {{
+ *   parsedSchema: DocumentNode,
+ *   validationErrors: GraphQLError[]
+ * }} The parsing result object containing parsed schema and validation errors
+ * @throws {GraphQLError} Throws if schema content cannot be parsed
+ */
+function parseSchema({ schemaContent }) {
+	if (!schemaContent) {
+		throw new Error('Schema is empty');
+	}
+
+	// TODO: validate before parsing
+	const validationErrors = [];
+
+	const parsedSchema = parse(schemaContent);
+
+	return { parsedSchema, validationErrors };
+}
+
+module.exports = {
+	parseSchema,
+};
---
+++ reverse_engineering/mappers/schema.js
@@ -0,0 +1,51 @@
+/**
+ * @import { DocumentNode } from "graphql"
+ * @import { Logger, FileREEntityResponseData } from "../types/types"
+ */
+
+const { parse, visit, Kind } = require('graphql');
+const { parseSchema } = require('../helpers/parseSchema');
+const { mapRootSchemaTypesToContainer } = require('./rootSchemaTypes');
+const { findNodesByKind } = require('../helpers/findNodesByKind');
+
+/**
+ * Maps a GraphQL schema to a RE response
+ * @param {Object} params
+ * @param {DocumentNode[]} params.schemaItems - The schema items
+ * @param {string} params.graphName - The name of the graph to be mapped as the container name
+ * @param {Logger} params.logger - The logger
+ * @returns {FileREEntityResponseData[]} The mapped entities
+ */
+function getMappedSchema({ schemaItems, graphName, logger }) {
+	try {
+		if (!schemaItems) {
+			throw new Error('Schema items are empty');
+		}
+		const container = mapRootSchemaTypesToContainer({
+			rootSchemaNode: findNodesByKind({ nodes: schemaItems, kind: Kind.SCHEMA_DEFINITION })[0],
+			graphName,
+		});
+
+		return [
+			// TODO: remove test collection
+			{
+				jsonSchema: '{ "type": "object", "operationType": "Query" }',
+				objectNames: {
+					collectionName: 'Test Collection',
+				},
+				doc: {
+					bucketInfo: container,
+					collectionName: 'Test Collection',
+					dbName: container.name,
+				},
+			},
+		];
+	} catch (error) {
+		logger.log('error', error, 'Failed to map GraphQL schema');
+		throw new Error(`Failed to map GraphQL schema: ${error.message}`);
+	}
+}
+
+module.exports = {
+	getMappedSchema,
+};
