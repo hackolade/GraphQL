ef585e6af4d19054be64342bd539c3330dd33fe2
Vitalii Bedletskyi <70570504+VitaliiBedletskyi@users.noreply.github.com>
2025-02-27 17:06:49 +0200

    HCK-10082: add complex test for generating full GraphQL schema (#83)
    * HCK-10082: add complex test for generating full GraphQL schema
    
    * adjust comment
    
    * adjust comparison schema behavior
    
    * fix typo

---
+++ test/forward_engineering/api.spec.js
@@ -0,0 +1,35 @@
+/**
+ * @import { Logger } from '../../types/types
+ */
+
+const { describe, it } = require('node:test');
+const fs = require('node:fs/promises');
+const { promisify } = require('node:util');
+const { strictEqual } = require('node:assert');
+const { generateContainerScript } = require('../../forward_engineering/api');
+const containerLevelShema = require('./containerLevelSchemaAsset');
+const path = require('node:path');
+
+const generateContainerScriptPromise = promisify(generateContainerScript);
+
+/**
+ * @type {Logger}
+ */
+const loggerMock = {
+	log: () => {},
+};
+
+const deleteSchemaVersionAndDate = script => script.split('\n').slice(3).join('\n');
+
+describe(() => {
+	it('should generate valid GraphQL schema from provided containerLevelSchema', async () => {
+		const result = await generateContainerScriptPromise(containerLevelShema, loggerMock);
+		const rawSchema = (await fs.readFile(path.join(__dirname, './expectedSchema.graphql'))).toString();
+
+		// remove schema version and date from the scripts to avoid differences
+		const resultSchema = deleteSchemaVersionAndDate(result);
+		const expectedSchema = deleteSchemaVersionAndDate(rawSchema);
+
+		strictEqual(resultSchema, expectedSchema);
+	});
+});
