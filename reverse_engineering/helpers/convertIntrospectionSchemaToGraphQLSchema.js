93eeee0d8e0cddc7f5476e7e531282edc07084ed
Vitalii Bedletskyi <vitalii.bedletskyi@hackolade.com>
2025-03-10 17:15:46 +0200

    add entry point for RE from GraphQL server

---
+++ reverse_engineering/helpers/convertIntrospectionSchemaToGraphQLSchema.js
@@ -0,0 +1,19 @@
+/**
+ * @import { IntrospectionQuery } from 'graphql';
+ */
+
+const { buildClientSchema, printSchema } = require('graphql/utilities');
+
+/**
+ * Convert introspection schema to GraphQL SDL schema
+ * @param {IntrospectionQuery} introspectionSchema
+ * @returns {string}
+ */
+function convertIntrospectionSchemaToGraphQLSchema(introspectionSchema) {
+	const schema = buildClientSchema(introspectionSchema);
+	return printSchema(schema);
+}
+
+module.exports = {
+	convertIntrospectionSchemaToGraphQLSchema,
+};
