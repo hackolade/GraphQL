c01296d73252370c1d29b6efba01728e20a5ac72
Vitalii Bedletskyi <vitalii.bedletskyi@hackolade.com>
2025-03-24 12:50:04 +0200

    remove console.log

--- reverse_engineering/mappers/directiveUsage.js
+++ reverse_engineering/mappers/directiveUsage.js
@@ -15,7 +15,6 @@ const { getDirectiveName } = require('./directiveName');
  * @returns {StructuredDirective[]} The mapped directives usage
  */
 function mapDirectivesUsage({ directives = [] }) {
-	console.log('mapDirectivesUsage');
 	return directives.map(directive => {
 		return {
 			directiveFormat: DIRECTIVE_FORMAT.structured,
