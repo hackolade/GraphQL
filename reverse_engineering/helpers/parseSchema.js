0e46cafa96bb3df73fd23180529f11657441d0c8
Taras Dubyk <tarass.dubyk@gmail.com>
2025-04-03 13:09:19 +0300

    add schema validation during RE (#102)

--- reverse_engineering/helpers/parseSchema.js
+++ reverse_engineering/helpers/parseSchema.js
@@ -3,6 +3,7 @@
  */
 
 const { parse } = require('graphql');
+const validationHelper = require('../../forward_engineering/helpers/schemaValidationHelper');
 
 /**
  * Parses GraphQL schema content into AST and validates it
@@ -11,22 +12,40 @@ const { parse } = require('graphql');
  * @param {string} params.schemaContent - The GraphQL schema content to parse
  * @returns {{
  * 	parsedSchema: DocumentNode;
- * 	validationErrors: GraphQLError[];
- * }} The parsing result object containing
- *   parsed schema and validation errors
- * @throws {GraphQLError} Throws if schema content cannot be parsed
+ * 	validationErrors: string[];
+ * }} The parsing result object containing parsed
+ *   schema and validation errors
+ * @throws {Error} Throws if schema content cannot be parsed
  */
 function parseSchema({ schemaContent }) {
 	if (!schemaContent) {
 		throw new Error('Schema is empty');
 	}
 
-	// TODO: validate before parsing
-	const validationErrors = [];
+	const validationErrors = validateSchema(schemaContent);
 
 	const parsedSchema = parse(schemaContent);
 
 	return { parsedSchema, validationErrors };
+}
+
+/**
+ * Validates the given schema content.
+ *
+ * @param {string} schemaContent - The schema content to validate.
+ * @returns {string[]} An array of validation error messages.
+ */
+function validateSchema(schemaContent) {
+	const validationResults = validationHelper.validate({ schema: schemaContent });
+
+	return validationResults
+		.filter(validationResult => validationResult.type !== 'success')
+		.map(validationError => {
+			if (validationError.label) {
+				return `${validationError.title}: ${validationError.label}`;
+			}
+			return validationError.title;
+		});
 }
 
 module.exports = {
